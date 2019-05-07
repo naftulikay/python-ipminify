8d3871fac380c32ca95ded0500903ec9c752939e
Naftuli Kay <me@naftuli.wtf>
2019-05-07 12:26:46 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+# python-ipminify [![Build Status][travis.svg]][travis]
+
+A Python implementation of IPv4 minification using a specified alphabet.
+
+## License
+
+Licensed at your discretion under either:
+
+ - [Apache Software License, Version 2.0](./LICENSE-APACHE)
+ - [MIT License](./LICENSE-MIT)
---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "ipminify",
+    version = "0.0.1",
+    packages = find_packages("src"),
+    package_dir = { "": "src"},
+    author = "Naftuli Kay",
+    author_email = "me@naftuli.wtf",
+    url = "https://github.com/naftulikay/python-ipminify",
+    install_requires = [
+        "setuptools",
+    ],
+    dependency_links = [],
+    entry_points = {
+        "console_scripts": [
+            # example: 'ipminify = ipminify:main'
+        ]
+    },
+)
